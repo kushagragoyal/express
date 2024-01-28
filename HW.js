3134b2304a67c8e407bf1ce5852d9168b0c93250
KUSHAGRA GOYAL <55806683+kushagragoyal@users.noreply.github.com>
2024-01-28 14:39:03 +0530

    Add files via upload

--- HW.js
+++ HW.js
@@ -1,9 +1,21 @@
-const http = require('http') 
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const routes = require('./routes')
+const app = express()
 
-// const server = http.createServer(routes)                 //Work fine with 1st method
+app.use(bodyParser.urlencoded({extended: false}))
 
-console.log(routes.someText)                                //Work fine with 2nd and 3rd method
-const server = http.createServer(routes.handler) 
-server.listen(4000)
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form')
+})
+
+app.use('/product',(req, res, next) => {
+    console.log(req.body)
+    res.redirect(`/`)
+})
+
+app.use('/',(req, res, next) => {
+    res.send(`<h1>Hellow from express</h1>`)
+})
+
+app.listen(4000)
